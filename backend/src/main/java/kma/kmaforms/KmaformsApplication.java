953795c84ee0dcb69022b1623e4d4804e836365a
maksym <maxslobodianyk@gmail.com>
2020-11-08 00:02:15 +0200

    Initial

---
+++ backend/src/main/java/kma/kmaforms/KmaformsApplication.java
@@ -0,0 +1,13 @@
+package kma.kmaforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmaformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmaformsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kma/kmaforms/KmaformsApplicationTests.java
@@ -0,0 +1,13 @@
+package kma.kmaforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmaformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
