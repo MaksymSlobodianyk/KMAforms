fc48f5b43b9d9f26ddcf1935c2af94f217d90bbc
maksym <maxslobodianyk@gmail.com>
2020-11-08 23:50:23 +0200

    Add routing and rout guards for landing and home pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+import {MainPageComponent} from './components/main-page/main-page.component';
+import {AuthGuard} from './guards/auth.guard';
+import {LandingGuard} from './guards/landing.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    component: LandingPageComponent,
+    canActivate: [LandingGuard]
+  },
+  {
+    path: 'home',
+    component: MainPageComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
